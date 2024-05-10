d553eb55c995316065537acd9db6bbc3600912c3
gustavocar05 <gustavocar05@gmail.com>
2024-05-10 11:39:42 -0300

    2° COMMIT

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String primeiroNome = "Gustavo";
+        String segundoNome = "Carmo";
+        
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
---
+++ src/edu/gustavo/sintaxejava/anatomiadeclasses/Aula1a6.java
@@ -0,0 +1,7 @@
+package edu.gustavo.sintaxejava.anatomiadeclasses;
+
+public class Aula1a6 {
+    public static void main(String[] args) {
+        
+    }
+}
